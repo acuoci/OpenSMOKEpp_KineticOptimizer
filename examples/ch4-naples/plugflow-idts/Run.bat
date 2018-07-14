0f0e3fe06289bca05c0eef60fa4dff2b03b27a9b
Alberto Cuoci <alberto.cuoci@polimi.it>
2018-07-14 13:12:24 +0200

    Added example: non adiabatic PFR from Naples

---
+++ examples/ch4-naples/plugflow-idts/Run.bat
@@ -0,0 +1,5 @@
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_PlugFlowReactor.exe" --input input.01.dic
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_PlugFlowReactor.exe" --input input.02.dic
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_PlugFlowReactor.exe" --input input.0025.dic
+"%OPENSMOKEPP_EXE_FOLDER%\OpenSMOKEpp_PlugFlowReactor.exe" --input input.0075.dic
+pause
